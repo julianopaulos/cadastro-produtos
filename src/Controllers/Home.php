209236371980fb673c2db27044abb1cc591a38f7
julianopaulos <julianopaulo.santos@hotmail.com>
2022-02-12 15:27:15 -0300

    acertando css

--- src/Controllers/Home.php
+++ src/Controllers/Home.php
@@ -9,15 +9,12 @@ class Home{
     private $twig;
     function __construct() {
         $this->loader = new FilesystemLoader("src/Views");
-        $this->twig = new Environment($this->loader/* , [
-            'cache' => 'App/compilation_cache',
-        ] */);
+        $this->twig = new Environment($this->loader);
     }
 
     function load() {
-
+        $header = $this->twig->load("header/header.php");
         $template = $this->twig->load("home/home.php.twig");
-        //echo $template->render(['title' => 'variables', 'go' => 'here']);
-        echo $template->render();
+        echo $template->render(['header' => $header->render(['BASE_URL' => BASE_URL, 'PAGE_NAME' => 'Listagem de Produtos']), 'BASE_URL_CSS' => BASE_URL."src/Views/home"]);       
     }
 }
---
+++ src/Views/header/header.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="{{BASE_URL}}/assets/style/global.css" />
+<div class="header">
+  <a href="#default" class="logo">{{PAGE_NAME}}</a>
+  <div class="header-right">
+    <a class="active" href="#home">Home</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
+  </div>
+</div>
